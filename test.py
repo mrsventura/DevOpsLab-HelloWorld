af5c272d0789a5219ca3caae945d5c32327dc54c
mariarsv <mariarsv@gmail.com>
2018-07-07 21:06:02 -0400

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Página do Lab do curso Devops Essentials da 4Linux - Aula 5")
 
 
 if __name__ == "__main__":
